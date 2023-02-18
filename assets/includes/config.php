ad6771b56a69996173244f8da88a75db0b939d03
thakuratul2 <pratapsinghatul111@gmail.com>
2023-02-18 21:31:07 +0530

    Update

--- assets/includes/config.php
+++ assets/includes/config.php
@@ -9,10 +9,10 @@ $sql_db_user = "root";
 // MySQL Database Password
 $sql_db_pass = "";
 // MySQL Database Name
-$sql_db_name = "brsoftsol_db";
+$sql_db_name = "atul_shopdb";
 
 // Site URL
-$site_url = "http://localhost/E-Commerce-Atul-Project-/";
+$site_url = "http://localhost/E-Commerce-Atul-Project-";
 // e.g (http://brsoftsol.com)
 
 ?>
